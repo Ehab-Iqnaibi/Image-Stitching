5ee7f3010f31fa6e86dda7b3c3840e2bd34ff430
Ehab-Iqnaibi <iqnaibi_ehab@ppu.edu>
2022-12-24 20:13:12 +0200

    E2

---
+++ Image Stitching.py
@@ -0,0 +1,7 @@
+import cv2 as cv
+import numpy as np
+
+
+
+img1 = cv.imread('D:/Document-E/Master-courses/Python/HW/test images/Jerusalem01.jpg', cv.IMREAD_COLOR)
+img2 = cv.imread('D:/Document-E/Master-courses/Python/HW/test images/Jerusalem02.jpg', cv.IMREAD_COLOR)
